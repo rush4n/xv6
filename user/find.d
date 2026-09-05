313eecd15a98fd8efc4695027fe1269f2e7cdb57
rushan <privaterushan@gmail.com>
2026-09-05 18:22:39 -0400

    clean flies

--- user/find.d
+++
@@ -1,2 +0,0 @@
-user/find.o: user/find.c kernel/types.h kernel/fcntl.h kernel/fs.h \
- kernel/stat.h kernel/param.h user/user.h
--- user/mytest.d
+++
@@ -1,1 +0,0 @@
-user/mytest.o: user/mytest.c kernel/types.h kernel/stat.h user/user.h
